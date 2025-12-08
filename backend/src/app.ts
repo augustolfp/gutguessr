b639e533c1edc1903e05bfc230d00100f953fcb4
Augusto Lopes <augustolfp@gmail.com>
2025-12-08 22:51:48 +0000

    feat: add hello world endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import "express-async-errors";
 import express from "express";
 import cors from "cors";
+import { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json(), cors());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Olá mundo!");
+    return;
+} );
 
 export default app;
