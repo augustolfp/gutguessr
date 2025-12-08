c06e9dcea229dc63189c7c8a6abef1d95eb03daf
Augusto Lopes <augustolfp@gmail.com>
2025-12-08 22:02:28 +0000

    build: configure vite to work inside dev container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+// https://docs.docker.com/guides/reactjs/develop/
+// https://vite.dev/config/preview-options
+// https://www.buildwithmatija.com/blog/configuring-development-containers-docker-guide
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  server: {
+    // Configurar o host para escutar todos os endereços:
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+  },
 });
