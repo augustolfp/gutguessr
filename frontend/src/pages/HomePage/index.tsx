1d53caef6aaf387d6db8a76605659deb63de90d9
Augusto Lopes <augustolfp@gmail.com>
2025-12-09 16:19:57 +0000

    feat: configure axios and API fetching from frontend

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
+import { api } from "../../config/axios";
+
 export default function HomePage() {
-  return <div>Home Page!</div>;
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    // O "ignore" utilizado abaixo é um pattern previsto na documentação oficial do React para fetching com useEffect, e sua função é evitar Race Conditions:
+    async function fetchLocation() {
+      const result = await api.get("/location");
+      if (!ignore) {
+        setLocation(result.data);
+      }
+    }
+
+    let ignore = false;
+    fetchLocation();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Home Page!</h1>
+      <div>{JSON.stringify(location)}</div>
+    </div>
+  );
 }
