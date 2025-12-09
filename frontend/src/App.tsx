63a9230b36baba7859db1d8516c81eb44139cdeb
Augusto Lopes <augustolfp@gmail.com>
2025-12-09 15:39:12 +0000

    feat: configure react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+
+import HomePage from "./pages/HomePage";
+
 export default function App() {
-    return (
-        <div>
-            Hello World!
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
