2e94f98687159151e1feaeaccb52206aa9127404
Augusto Lopes <augustolfp@gmail.com>
2025-12-09 15:57:01 +0000

    feat: configure errorHandlerMW and router on backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,15 @@
 import "express-async-errors";
 import express from "express";
 import cors from "cors";
-import { Request, Response } from "express";
+
+import router from "./routes/index.js";
+import errorHandlerMW from "./middlewares/errorHandlerMW.js";
 
 const app = express();
 
 app.use(express.json(), cors());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Olá mundo!");
-    return;
-} );
+app.use(router);
+app.use(errorHandlerMW);
 
 export default app;
---
+++ backend/src/middlewares/errorHandlerMW.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../helpers/api-errors.js";
+
+export default function errorHandlerMW(
+  err: Error & Partial<ApiError>,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log("ERROR HANDLING MIDDLEWARE: ", err);
+  const statusCode = err.statusCode ?? 500;
+  const message = err.statusCode ? err.message : "Internal Server Error";
+  res.status(statusCode).json({ message });
+
+  return;
+}
