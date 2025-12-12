a7a5b829eabdbefff79184344b8b7008d1b0402a
Augusto Lopes <augustolfp@gmail.com>
2025-12-12 20:45:18 +0000

    feat: create MapPage component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 import HomePage from "./pages/HomePage";
+import MapPage from "./pages/MapPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/map-example" element={<MapPage />} />
       </Routes>
     </Router>
   );
