eb0f5f9b8282a313f7dad3480685383eed95c6fc
Augusto Lopes <augustolfp@gmail.com>
2025-12-14 19:03:23 +0000

    feat: add streetView update without re-renders

--- frontend/src/pages/MapPage/index.tsx
+++ frontend/src/pages/MapPage/index.tsx
@@ -6,11 +6,34 @@ setOptions({
   key: `${import.meta.env.VITE_GOOGLE_CLOUD_API_KEY}`,
 });
 
+// const locations = [
+//   {
+//     lat: 18.432081425952404,
+//     lng: 103.42956027000548,
+//     heading: 307,
+//     pitch: 0,
+//   },
+//   {
+//     lat: 35.06281288328733,
+//     lng: 134.20490283181627,
+//     heading: 193,
+//     pitch: 0,
+//   },
+//   {
+//     lat: 30.409635521971207,
+//     lng: -91.138758669815,
+//     heading: 118,
+//     pitch: 0,
+//   },
+// ];
+
 await importLibrary("maps");
 await importLibrary("streetView");
 
 export default function MapPage() {
   useEffect(() => {
+    const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
     const streetViewOptions: google.maps.StreetViewPanoramaOptions = {
       position: {
         lat: 18.432081425952404,
@@ -24,17 +47,26 @@ export default function MapPage() {
     };
 
     async function setupMaps() {
-      new google.maps.StreetViewPanorama(
+      const panorama = new google.maps.StreetViewPanorama(
         document.getElementById("panorama") as HTMLElement,
         streetViewOptions
       );
+
+      await sleep(10000);
+
+      console.log("Atualizando posição do panorama...");
+
+      panorama.setPosition({
+        lat: 35.06281288328733,
+        lng: 134.20490283181627
+      });
     }
 
     setupMaps();
   }, []);
   return (
     <div className="w-full h-screen bg-pink-300 p-4">
-        <div id="panorama" className="w-full h-full" />
+      <div id="panorama" className="w-full h-full" />
     </div>
   );
 }
