424e863df99af9c23791150d56e84555ba5ea387
Augusto Lopes <augustolfp@gmail.com>
2025-12-14 18:31:24 +0000

    feat: render street view with google api

---
+++ frontend/src/config/googleMapsApiConfig.ts
@@ -0,0 +1,10 @@
+export const streetViewBaseConfig: google.maps.StreetViewPanoramaOptions = {
+    addressControl: false,
+    disableDefaultUI: true,
+    motionTracking: false,
+    motionTrackingControl: false,
+    showRoadLabels: false,
+    zoomControlOptions: {
+        position: 6
+    } 
+};
--- frontend/src/pages/MapPage/index.tsx
+++ frontend/src/pages/MapPage/index.tsx
@@ -1,5 +1,40 @@
+import { useEffect } from "react";
+import { setOptions, importLibrary } from "@googlemaps/js-api-loader";
+import { streetViewBaseConfig } from "../../config/googleMapsApiConfig";
+
+setOptions({
+  key: `${import.meta.env.VITE_GOOGLE_CLOUD_API_KEY}`,
+});
+
+await importLibrary("maps");
+await importLibrary("streetView");
+
 export default function MapPage() {
-    return (
-        <div>Map Page</div>
-    );
+  useEffect(() => {
+    const streetViewOptions: google.maps.StreetViewPanoramaOptions = {
+      position: {
+        lat: 18.432081425952404,
+        lng: 103.42956027000548,
+      },
+      pov: {
+        heading: 307,
+        pitch: 0,
+      },
+      ...streetViewBaseConfig,
+    };
+
+    async function setupMaps() {
+      new google.maps.StreetViewPanorama(
+        document.getElementById("panorama") as HTMLElement,
+        streetViewOptions
+      );
+    }
+
+    setupMaps();
+  }, []);
+  return (
+    <div className="w-full h-screen bg-pink-300 p-4">
+        <div id="panorama" className="w-full h-full" />
+    </div>
+  );
 }
