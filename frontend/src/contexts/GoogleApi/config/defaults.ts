e8ccf7072a3f800f99c47a0187299f9076ee19d3
Augusto Lopes <augustolfp@gmail.com>
2025-12-15 21:37:46 +0000

    refactor: move maps default configs into separate file

---
+++ frontend/src/contexts/GoogleApi/config/defaults.ts
@@ -0,0 +1,26 @@
+const genericPosition: google.maps.LatLngLiteral = {
+  lat: -22.41422237388817,
+  lng: -45.45034290777719,
+};
+
+const genericPov: google.maps.StreetViewPov = {
+  heading: 0,
+  pitch: 0,
+};
+
+const streetViewBaseConfig: google.maps.StreetViewPanoramaOptions = {
+  addressControl: false,
+  disableDefaultUI: true,
+  motionTracking: false,
+  motionTrackingControl: false,
+  showRoadLabels: false,
+  zoomControlOptions: {
+    position: 6,
+  },
+};
+
+export {
+  genericPosition,
+  genericPov,
+  streetViewBaseConfig,
+};
--- frontend/src/contexts/GoogleApi/config/index.ts
+++ frontend/src/contexts/GoogleApi/config/index.ts
@@ -1,49 +1,27 @@
 import { setOptions, importLibrary } from "@googlemaps/js-api-loader";
+import * as defaults from "./defaults";
 
 setOptions({
   key: `${import.meta.env.VITE_GOOGLE_CLOUD_API_KEY}`,
 });
 
-const genericPosition: google.maps.LatLngLiteral = {
-  lat: -22.41422237388817,
-  lng: -45.45034290777719,
-};
-
-const genericPov: google.maps.StreetViewPov = {
-  heading: 0,
-  pitch: 0
-}
-
-const streetViewBaseConfig: google.maps.StreetViewPanoramaOptions = {
-  addressControl: false,
-  disableDefaultUI: true,
-  motionTracking: false,
-  motionTrackingControl: false,
-  showRoadLabels: false,
-  zoomControlOptions: {
-    position: 6,
-  },
-};
-
 await importLibrary("maps");
 await importLibrary("streetView");
 
 const loadStreetViewPanorama = async (
-  position: google.maps.LatLngLiteral = genericPosition,
-  pov: google.maps.StreetViewPov = genericPov
+  position: google.maps.LatLngLiteral = defaults.genericPosition,
+  pov: google.maps.StreetViewPov = defaults.genericPov
 ) => {
   const panorama = new google.maps.StreetViewPanorama(
     document.getElementById("panorama") as HTMLElement,
     {
       position,
       pov,
-      ...streetViewBaseConfig,
+      ...defaults.streetViewBaseConfig,
     }
   );
 
   return panorama;
 };
 
-export {
-    loadStreetViewPanorama
-};
+export { loadStreetViewPanorama };
