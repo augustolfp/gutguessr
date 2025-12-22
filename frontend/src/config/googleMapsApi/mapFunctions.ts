5e41f853e24faf078e10ed0c988b008f45cd38c8
Augusto Lopes <augustolfp@gmail.com>
2025-12-22 20:18:07 +0000

    feat: implement distance line tracing and zoom to fit functions for map

--- frontend/src/config/googleMapsApi/mapFunctions.ts
+++ frontend/src/config/googleMapsApi/mapFunctions.ts
@@ -9,4 +9,31 @@ const init = async () => {
   return newMap;
 };
 
-export { init };
+const traceDistanceLine = (
+  map: google.maps.Map,
+  positionA: google.maps.LatLngLiteral,
+  positionB: google.maps.LatLngLiteral
+) => {
+  const distanceLine = new google.maps.Polyline({
+    path: [positionA, positionB],
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  distanceLine.setMap(map);
+};
+
+const adjustZoomToFitPoints = async (
+  map: google.maps.Map,
+  positionA: google.maps.LatLngLiteral,
+  positionB: google.maps.LatLngLiteral
+) => {
+  const bounds = new google.maps.LatLngBounds();
+  bounds.extend(positionA);
+  bounds.extend(positionB);
+  map.fitBounds(bounds);
+};
+
+export { init, traceDistanceLine, adjustZoomToFitPoints };
