dabe1c601dd21acf62ffe67712f10dcba3825127
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-12-27 19:09:00 +0000

    feat: remove locations router, controller and repository

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 
-import locationsRouter from "./locationsRouter";
 import quizzesRouter from "./quizzesRouter";
 
 const router = Router();
 
-router.use(locationsRouter);
 router.use(quizzesRouter);
 
 export default router;
