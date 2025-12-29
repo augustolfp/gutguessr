af6c32a6dcfaa313cecf0f9015bc8797d67dd2c8
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-12-29 02:43:05 -0300

    feat: add complete stats response for guesses endpoints

--- backend/src/controllers/guessesController.ts
+++ backend/src/controllers/guessesController.ts
@@ -1,20 +1,28 @@
 import { Request, Response } from "express";
 import * as guessesService from "../services/guessesService";
+import * as gameSessionsService from "../services/gameSessionsService";
 
 export async function declareRoundTimeout(req: Request, res: Response) {
   const roundId = req.body.roundId;
+  const gameSessionId = req.body.gameSessionId;
 
-  const timeoutInfo = await guessesService.declareRoundTimeout(roundId);
+  await guessesService.declareRoundTimeout(roundId);
+  const updatedGameSession = await gameSessionsService.getGameSessionDetails(
+    gameSessionId
+  );
 
-  res.status(201).json(timeoutInfo);
+  res.status(201).json(updatedGameSession);
   return;
 }
 
 export async function savePlayerRoundGuess(req: Request, res: Response) {
-  const { roundId, lat, lng } = req.body;
+  const { roundId, gameSessionId, lat, lng } = req.body;
 
-  const newGuess = await guessesService.savePlayerRoundGuess(roundId, lat, lng);
+  await guessesService.savePlayerRoundGuess(roundId, lat, lng);
+  const updatedGameSession = await gameSessionsService.getGameSessionDetails(
+    gameSessionId
+  );
 
-  res.status(201).json(newGuess);
+  res.status(201).json(updatedGameSession);
   return;
 }
